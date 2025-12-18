b67de9b0ce9ae2b7cba9d7be1827496a776d2099
BLUE <isyouyoung@gmail.com>
2025-12-18 20:23:22 +0900

    이건 너무 쉬워서 설명 필요 X

---
+++ src/test_1330/NumberComparer.java
@@ -0,0 +1,26 @@
+package test_1330;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NumberComparer {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(line);
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
