d51cfb173c2051fae25e41676f6e483df883b461
BLUE <isyouyoung@gmail.com>
2025-12-21 12:04:31 +0900

    등차수열의 합 공식으로 푼것 말도안되 ㅋㅋㅋㅋ

--- src/test_8393/SumUpToN.java
+++ src/test_8393/SumUpToN.java
@@ -13,10 +13,18 @@ public class SumUpToN {
         String line = bf.readLine();
         int n = parseInt(line);
         int hap = 0;
+//
+//        for (int i = 1; i <= n; i++) {
+//            hap = hap + i;
+//        }
+//        System.out.println(hap);
 
-        for (int i = 1; i <= n; i++) {
-            hap = hap + i;
-        }
+        hap = n * (n + 1 ) / 2;
         System.out.println(hap);
     }
 }
+
+// AI가 추천해준 공식
+// 가우스가 발견한 등차수열의 합 공식이라고한다
+// ㅋㅋ 이게 된다고?
+// ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
