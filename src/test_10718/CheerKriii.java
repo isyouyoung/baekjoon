d39ad9770d5dc7f2a9d16387cfc2a7afa14e51d9
BLUE <isyouyoung@gmail.com>
2025-12-21 12:09:29 +0900

    뭐야 이 문제는 ㅋㅋㅋㅋㅋ 그냥 두번 출력이다

---
+++ src/test_10718/CheerKriii.java
@@ -0,0 +1,10 @@
+package test_10718;
+
+public class CheerKriii {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 3; i++) {
+            System.out.println("강한친구 대한육군");
+        }
+    }
+}
