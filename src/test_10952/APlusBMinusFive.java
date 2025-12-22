ea6b55a384bfbb4727c945e017abc9c1ed82f8e8
isyouyoung <isyouyoung@gmail.com>
2025-12-22 15:24:31 +0900

    1

---
+++ src/test_10952/APlusBMinusFive.java
@@ -0,0 +1,20 @@
+package test_10952;
+
+import java.util.Scanner;
+
+public class APlusBMinusFive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int A  = sc.nextInt();
+            int B  = sc.nextInt();
+
+            if (A == 0 && B == 0) break;
+
+            int result = A + B;
+            System.out.println(result);
+        }
+
+    }
+}
