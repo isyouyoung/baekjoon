6962b9834876295a5619ce48f74f12a91033da7a
BLUE <isyouyoung@gmail.com>
2025-12-23 23:11:32 +0900

    공부해서 수정한버전 오래걸렸음;;

--- src/test_10952/APlusBMinusFive.java
+++ src/test_10952/APlusBMinusFive.java
@@ -1,9 +1,27 @@
 package test_10952;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class APlusBMinusFive {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String line = bf.readLine();
+
+        StringTokenizer st =  new StringTokenizer(line);
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        String readLine = bf.readLine();
+        StringTokenizer number =  new StringTokenizer(readLine);
+
+        for (int i = 0; i < N; i++) {
+            int c = Integer.parseInt(number.nextToken());
+            if ( c < X ) {
+                System.out.print(c + " ");
+            }
+        }
     }
 }
