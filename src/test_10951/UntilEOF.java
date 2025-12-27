1f9d2c188dd9bde9374074ffdddf923661d0d64b
BLUE <isyouyoung@gmail.com>
2025-12-28 00:43:12 +0900

    오늘 풀것

---
+++ src/test_10951/UntilEOF.java
@@ -0,0 +1,12 @@
+package test_10951;
+
+public class UntilEOF {
+    public static void main(String[] args) {
+        
+    }
+}
+
+// 이번에도 더하는 문제인대 문제는 끝?이 안보인다
+// 즉 몇개가 들어올지 알수가 없다 이게 핵심인듯
+// 그럼 for 문보다는 while문 => 조건을 모르므로 사용하면될듯하다
+// 아무것도 안들어오면 (null)이면 종료 이런식으로?
