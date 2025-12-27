a7b3f598a6790fe88b400d85265bd479576010d3
BLUE <isyouyoung@gmail.com>
2025-12-28 00:53:07 +0900

    오늘 푼것 (정답)

--- src/test_10951/UntilEOF.java
+++ src/test_10951/UntilEOF.java
@@ -1,8 +1,24 @@
 package test_10951;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class UntilEOF {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String line = bf.readLine();
+            if (line == null) { break; }
+                StringTokenizer st = new StringTokenizer(line);
+                int A = Integer.parseInt(st.nextToken());
+                int B = Integer.parseInt(st.nextToken());
+                System.out.println(A+B);
+        }
+
     }
 }
 
