5c22fc67132b2d347c8e14016ce42fda3ead1759
BLUE <isyouyoung@gmail.com>
2025-12-27 23:35:41 +0900

    오늘 풀것

--- src/test_11021/SumFormatter.java
+++ src/test_11021/SumFormatter.java
@@ -3,5 +3,15 @@ package test_11021;
 public class SumFormatter {
     public static void main(String[] args) {
 
+
     }
 }
+
+// 구조는 기존 더하기랑 비슷한듯
+// 목표는 ai 안쓰고 풀어보기
+// 일단 적어보면
+// bf로 받아서 바이트데이터?를 문자데이터로 바꾸고 그걸 버퍼에 저장한다음
+// readLine으로 한줄 읽고 int로 바꾼다음에
+// 두수를 토크나이저로 쪼개서 더하는대
+// 그냥 앞에 Case #1 이렇게 붙이면 될듯
+// 다만 1.2.3.4.5 이렇게 있는건 반복문을 사용하면 될거같음
