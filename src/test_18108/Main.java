2d99501aaf73807edda993a7e520a4c7744af8fb
isyouyoung <isyouyoung@gmail.com>
2025-12-29 14:30:50 +0900

    예상

---
+++ src/test_18108/Main.java
@@ -0,0 +1,11 @@
+package test_18108;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+// y가 입력되면 (주어지면)
+// 그걸 불기를 서기년도로 바꿔서 출력만하면 되는 간단한 문제같음
+// 다만 내가 어떻게 불기를 서기년도로 바꾸는지 모르기 때문에 그것만 물어봐야할듯
