4e3be7c67120b07b0f6a21fc599a40a7055622b7
BLUE <isyouyoung@gmail.com>
2026-01-01 12:51:01 +0900

    오늘 푼것 (정답)

--- src/test_2741/NPrinter.java
+++ src/test_2741/NPrinter.java
@@ -1,9 +1,22 @@
 package test_2741;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class NPrinter {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String line = bf.readLine();
+        int N = Integer.parseInt(line);
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(i+1);
+        }
     }
+    
 }
 
 // 별찍기 문제 생성
+// 이리하면 되나? 일단 제출해보자
