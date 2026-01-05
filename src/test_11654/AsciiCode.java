d29d21f8140b755e0f2cd00fa30ae3e6515d93c9
isyouyoung <isyouyoung@gmail.com>
2026-01-05 19:00:08 +0900

    푸는중

---
+++ src/test_11654/AsciiCode.java
@@ -0,0 +1,7 @@
+package test_11654;
+
+public class AsciiCode {
+    public static void main(String[] args) {
+
+    }
+}
--- src/test_2742/ReverseNPrinter.java
+++ src/test_2742/ReverseNPrinter.java
@@ -1,8 +1,17 @@
 package test_2742;
-// 기찍 N
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ReverseNPrinter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String line = bf.readLine();
+        int N = Integer.parseInt(line);
+
+        System.out.println(N);
 
     }
 }
