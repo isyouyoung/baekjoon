d23f9e0507c71f8793eeea65e864ba7bf80e4283
isyouyoung <isyouyoung@gmail.com>
2026-01-06 11:46:53 +0900

    풀것 (푸는중)

--- src/test_11382/BigSum.java
+++ src/test_11382/BigSum.java
@@ -1,8 +1,13 @@
 package test_11382;
 // 꼬마 정민
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class BigSum {
     public static void main(String[] args) {
+
+        BufferedReader bf = new BufferedReader(InputStreamReader(System.in));
 
 
     }
