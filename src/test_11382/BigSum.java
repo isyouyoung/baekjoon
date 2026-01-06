1bc471d77e1ad071a034c2247355b77cf52c7d76
isyouyoung <isyouyoung@gmail.com>
2026-01-06 17:55:50 +0900

    푼것 (완성)

--- src/test_11382/BigSum.java
+++ src/test_11382/BigSum.java
@@ -2,13 +2,21 @@ package test_11382;
 // 꼬마 정민
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BigSum {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        BufferedReader bf = new BufferedReader(InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
 
+        StringTokenizer st = new StringTokenizer(line);
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
+        System.out.println(A+B+C);
     }
 }
