021e44c50acccc1f1c26ce099310602fafc348aa
isyouyoung <isyouyoung@gmail.com>
2026-01-07 15:12:43 +0900

    제출 (정답)

--- src/test_10807/CountingV.java
+++ src/test_10807/CountingV.java
@@ -1,8 +1,36 @@
 package test_10807;
 
-public class CountingV {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class CountingV {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int N = Integer.parseInt(line);
+        int[] B = new int[N];
+        String numbersLine = br.readLine();
+        StringTokenizer st = new StringTokenizer(numbersLine);
+
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        String foundNumberLine = br.readLine();
+        int foundNumber = Integer.parseInt(foundNumberLine);
+
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (B[i] == foundNumber) {
+                count ++;
+            }
+        }
+
+        System.out.println(count);
     }
 }
 
