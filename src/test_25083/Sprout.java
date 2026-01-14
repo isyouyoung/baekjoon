f06872110a5f50dac326817b6be103283c182114
isyouyoung <isyouyoung@gmail.com>
2026-01-14 22:17:45 +0900

    공부할것 그냥새싹출력...?

--- src/test_25083/Sprout.java
+++ src/test_25083/Sprout.java
@@ -1,4 +1,12 @@
 package test_25083;
 
 public class Sprout {
+    public static void main(String[] args) {
+     //          ,r'"7
+        //r`-_   ,'  ,/
+        // \. ". L_r'
+        //   `~\/
+        //      |
+        //      |
+    }
 }
