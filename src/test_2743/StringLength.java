571e0f889ed7b226852f094ecff341be77520760
isyouyoung <isyouyoung@gmail.com>
2026-01-17 23:07:57 +0900

    공부할것

--- src/test_2743/StringLength.java
+++ src/test_2743/StringLength.java
@@ -2,3 +2,11 @@ package test_2743;
 
 public class StringLength {
 }
+
+/*
+ * [백준 2743번: 단어 길이 재기 요약]
+ * 1. 핵심: 입력받은 문자열의 길이를 계산하여 출력하기.
+ * 2. 메서드: String 클래스의 .length() 메서드를 활용.
+ * 3. 주의: 배열의 길이는 array.length(괄호 없음)이지만, 문자열의 길이는 string.length()(괄호 있음)임.
+ * 4. 제출: 클래스명은 Main, 패키지 선언 삭제 필수.
+ */
