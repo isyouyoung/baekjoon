9a0c302f6d0e4192edfea35c0c059c4da8e4d792
BLUE <isyouyoung@gmail.com>
2026-01-19 21:29:07 +0900

    오늘 푼것

--- src/test_25083/Sprout.java
+++ src/test_25083/Sprout.java
@@ -2,6 +2,12 @@ package test_25083;
 
 public class Sprout {
     public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
      //          ,r'"7
         //r`-_   ,'  ,/
         // \. ". L_r'
@@ -10,3 +16,5 @@ public class Sprout {
         //      |
     }
 }
+
+// 띄어쓰기 하나 더 되어서 첫번째 9갠대 10개 해서 틀렸음;;
