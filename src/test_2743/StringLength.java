08d87ce104c3b7754627ac80eaa40ad552c9954f
BLUE <isyouyoung@gmail.com>
2026-01-19 23:36:54 +0900

    푸는중

--- src/test_2743/StringLength.java
+++ src/test_2743/StringLength.java
@@ -1,6 +1,17 @@
 package test_2743;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class StringLength {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+
+        System.out.println(line);
+    }
 }
 
 /*
