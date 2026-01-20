0c1a121be34f8dc7afc65d5519a639c389ef051f
BLUE <isyouyoung@gmail.com>
2026-01-20 23:48:47 +0900

    설명추가 (요약추가)

--- src/test_2558/SumAB2.java
+++ src/test_2558/SumAB2.java
@@ -18,3 +18,15 @@ public class SumAB2 {
 
     }
 }
+
+//         String line1 = br.readLine();
+//        String line2 = br.readLine(); 이렇게 붙여써도 전혀 상관없음 입력받은 첫번째 줄은 line1이 되는거고 두번째 줄은 line2가 되는것뿐
+
+// System.out.println(Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine())); 고수들은 이렇게도 한다고함 한번에 출력
+
+/*
+ * [백준 2558번: A+B - 2 핵심 로직]
+ * 1. br.readLine()은 호출될 때마다 한 줄씩 아래로 내려가며 데이터를 읽어온다.
+ * 2. 따라서 두 줄을 각각 더하려면 readLine()을 두 번 실행하여 각각의 변수에 담아야 한다.
+ * 3. 읽어온 값은 String이므로 연산을 위해 반드시 Integer.parseInt() 처리가 필요하다.
+ */
