ec8fd865df20c6ef02ad5bd335b1a781f745016c
BLUE <isyouyoung@gmail.com>
2026-01-20 23:56:31 +0900

    푼것 일단 노가다로...

--- src/test_3003/ChessPieces.java
+++ src/test_3003/ChessPieces.java
@@ -1,7 +1,31 @@
 package test_3003;
 
-public class ChessPieces {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class ChessPieces {
+    public static void main(String[] args) throws IOException {
+
+        int[] origin = {1, 1, 2, 2, 2, 8};
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(line);
+        int king = Integer.parseInt(st.nextToken());
+        int queen = Integer.parseInt(st.nextToken());
+        int rook = Integer.parseInt(st.nextToken());
+        int bishop = Integer.parseInt(st.nextToken());
+        int knight = Integer.parseInt(st.nextToken());
+        int pawn = Integer.parseInt(st.nextToken());
+
+        System.out.print((origin[0] - king) + " ");
+        System.out.print((origin[1] - queen) + " ");
+        System.out.print((origin[2] - rook) + " ");
+        System.out.print((origin[3] - bishop) + " ");
+        System.out.print((origin[4] - knight) + " ");
+        System.out.print((origin[5] - pawn));
     }
 }
