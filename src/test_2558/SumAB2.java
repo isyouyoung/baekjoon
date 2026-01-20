7b65b82329e3960fc0587defba6baca8477c32af
BLUE <isyouyoung@gmail.com>
2026-01-20 23:47:13 +0900

    정답 제출

--- src/test_2558/SumAB2.java
+++ src/test_2558/SumAB2.java
@@ -1,8 +1,20 @@
 package test_2558;
 
-public class SumAB2 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class SumAB2 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line1 = br.readLine();
+        String line2 = br.readLine();
+
+        int A = Integer.parseInt(line1);
+        int B = Integer.parseInt(line2);
+
+        System.out.println(A + B);
 
     }
 }
