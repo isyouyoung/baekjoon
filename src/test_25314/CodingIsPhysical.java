747e1d01976180ab0c5e18c8660d0b4e65eff014
BLUE <isyouyoung@gmail.com>
2026-01-21 20:56:33 +0900

    푸는중

---
+++ src/test_25314/CodingIsPhysical.java
@@ -0,0 +1,16 @@
+package test_25314;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CodingIsPhysical {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        System.out.println(line);
+
+
+    }
+}
