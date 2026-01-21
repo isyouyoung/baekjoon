3475d14b790bb7a6f758bc4d569ccc050c7239bb
BLUE <isyouyoung@gmail.com>
2026-01-22 00:49:37 +0900

    제출 (답)

--- src/test_27866/StringIndex.java
+++ src/test_27866/StringIndex.java
@@ -1,10 +1,20 @@
 package test_27866;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class StringIndex {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        // 일단 먼저 제출
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line1 = br.readLine();
+        String line2 = br.readLine();
+        int i = Integer.parseInt(line2);
+        char s = line1.charAt(i - 1);
 
+        System.out.println(s);
 
     }
 }
