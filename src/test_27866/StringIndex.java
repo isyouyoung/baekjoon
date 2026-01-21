92e8ded0507511b121fd3ea62de7cf41cff7706c
BLUE <isyouyoung@gmail.com>
2026-01-22 00:49:44 +0900

    제출 (답)

--- src/test_27866/StringIndex.java
+++ src/test_27866/StringIndex.java
@@ -3,7 +3,6 @@ package test_27866;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class StringIndex {
     public static void main(String[] args) throws IOException {
