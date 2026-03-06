cebd732addd5caab073aa31d3569ac096c5ca9a3
isyouyoung <isyouyoung@gmail.com>
2026-03-06 13:22:51 +0900

    10757번 문제 BigNumberSum 사용

---
+++ src/test_10757/BigNumberSum.java
@@ -0,0 +1,20 @@
+package test_10757;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BigNumberSum {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        System.out.println(A.add(B));
+    }
+}
