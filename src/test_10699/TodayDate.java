6adbed0508faaf8ac34f1b837911dee0e783379a
isyouyoung <isyouyoung@gmail.com>
2026-03-10 20:15:39 +0900

    푼것

--- src/test_10699/TodayDate.java
+++ src/test_10699/TodayDate.java
@@ -1,7 +1,11 @@
 package test_10699;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class TodayDate {
     public static void main(String[] args) {
-
+        System.out.println(LocalDate.now());
+//        System.out.println(LocalDateTime.now());
     }
 }
